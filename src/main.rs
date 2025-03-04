84f3eac1ddc23b80567f14041addead6555ad686
Zatzou <zatzou@zatzou.com>
2025-03-04 22:27:31 +0200

    add comments

--- src/main.rs
+++ src/main.rs
@@ -195,6 +195,7 @@ pub fn App() -> impl IntoView {
             </div>
         </ShitMap>
 
+        // sidebar open button
         <div on:click={move |_| set_show_sidebar.set(!show_sidebar.get())} class="fixed top-0 left-0 p-2 cursor-pointer z-50 bg-neutral-900 rounded-e-full mt-2 p-2">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 text-white">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
@@ -202,9 +203,12 @@ pub fn App() -> impl IntoView {
         </div>
         // class="-translate-x-full"
         <div class="flex flex-col bg-neutral-900 w-full max-w-full h-screen z-50 absolute top-0 md:max-w-sm transition-transform text-white" class:-translate-x-full={move || !show_sidebar.get()}>
+            // top text
             <div>
                 <div class="flex justify-between p-2 items-center">
                     <h1 class="text-4xl">Wynnmap</h1>
+
+                    // close button
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8 cursor-pointer" on:click={move |_| set_show_sidebar.set(!show_sidebar.get())}>
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
                     </svg>
@@ -212,6 +216,7 @@ pub fn App() -> impl IntoView {
                 <hr class="border-neutral-600" />
             </div>
 
+            // checkboxes
             <div class="flex-1 flex flex-col p-2 text-lg">
                 <Checkbox id="terrs" checked={show_terrs}>"Territories"</Checkbox>
                 <Checkbox id="conns" checked={show_conns}>"Connections"</Checkbox>
@@ -219,6 +224,7 @@ pub fn App() -> impl IntoView {
                 <Checkbox id="timers" checked={show_timers}>"Timers"</Checkbox>
             </div>
 
+            // guild leaderboard
             <div class="flex flex-col min-h-0">
                 <hr class="border-neutral-600" />
                 <div class="flex justify-between items-center text-xl p-2 py-1" on:click={move |_| show_guild_leaderboard.set(!show_guild_leaderboard.get())}>
@@ -234,6 +240,7 @@ pub fn App() -> impl IntoView {
                 </div>
             </div>
 
+            // bottom text
             <div>
                 <hr class="border-neutral-600" />
                 <h2 class="text-neutral-500 p-1 px-2"><a class="underline" href="https://github.com/Zatzou/wynnmap" target="_blank">"Wynnmap"</a>" "{env!("CARGO_PKG_VERSION")}</h2>
