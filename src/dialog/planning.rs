3918292d05e32169a331e83f126d110e8f91d70a
Zatzou <zatzou@zatzou.com>
2025-04-21 01:29:48 +0300

    edit and list fixes

--- src/dialog/planning.rs
+++ src/dialog/planning.rs
@@ -51,28 +51,41 @@ pub fn manage_guilds(guilds: RwSignal<Vec<Guild>>) -> impl IntoView {
                             <th>"Tag"</th>
                             <th>"Name"</th>
                             <th>"Color"</th>
+                            <th/>
                         </tr>
                     </thead>
                     <tbody>
                         <ForEnumerate
                             each=move || guilds.get()
                             key=|guild| guild.clone()
-                            children=|idx, guild| {
+                            children=move |idx, guild| {
                                 view! {
                                     <tr>
                                         <td class="p-2 font-mono">"["{guild.prefix}"]"</td>
                                         <td class="p-2">{guild.name}</td>
                                         <td>{guild.color}</td>
                                         <td class="flex">
-                                            // edit button
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                                            </svg>
+                                            <Show when={move || idx.get() != 0}>
+                                                // edit button
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"
+                                                    on:click={
+                                                        let owner = Owner::new();
+                                                        let idx = idx.get();
+                                                        move |_| {
+                                                            owner.with(move || {
+                                                                show_dialog(move || edit_guild(guilds, idx as u8));
+                                                            });
+                                                        }
+                                                    }
+                                                >
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
 
-                                            // delete button
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                                            </svg>
+                                                // delete button
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                                </svg>
+                                            </Show>
                                         </td>
                                     </tr>
                                 }
@@ -88,7 +101,7 @@ pub fn manage_guilds(guilds: RwSignal<Vec<Guild>>) -> impl IntoView {
 fn add_guild(guilds: RwSignal<Vec<Guild>>) -> impl IntoView {
     let tag = RwSignal::new(String::new());
     let name = RwSignal::new(String::new());
-    let color = RwSignal::new(String::new());
+    let color = RwSignal::new(String::from("#FFFFFF"));
 
     view! {
         <div class="bg-neutral-900 md:rounded-xl text-white flex flex-col">
@@ -171,14 +184,17 @@ fn edit_guild(guilds: RwSignal<Vec<Guild>>, n: u8) -> impl IntoView {
                     move |_| {
                         // TODO: validate inputs
                         guilds.update(|guilds| {
-
-                            // TODO: update guild
+                            if let Some(guild) = guilds.get_mut(n as usize) {
+                                guild.prefix = Some(Arc::from(tag.get()));
+                                guild.name = Some(Arc::from(name.get()));
+                                guild.color = Some(Arc::from(color.get()));
+                            }
                         });
 
                         close_dialog(dialogs);
                     }
                 }>
-                    "Add guild"
+                    "Save"
                 </button>
             </div>
         </div>
