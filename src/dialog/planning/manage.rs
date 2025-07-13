aec28c4a13d4ef7d8b1a2e1e2bb73ec93c0e9959
Zatzou <zatzou@zatzou.com>
2025-07-13 23:54:05 +0300

    Reapply "refactor planning stuff and guild deletion"
    This reverts commit 4489ec55f65104ea2149d3cbdc442e41c0091402.

--- src/dialog/planning/manage.rs
+++ src/dialog/planning/manage.rs
@@ -7,7 +7,7 @@ use crate::dialog::{
     show_dialog,
 };
 
-pub fn manage_guilds(guilds: RwSignal<Vec<Guild>>) -> impl IntoView {
+pub fn manage_guilds(guilds: RwSignal<Vec<ArcRwSignal<Guild>>>) -> impl IntoView {
     view! {
         <div class="bg-neutral-900 md:rounded-xl text-white w-screen max-w-3xl h-dvh md:max-h-150 flex flex-col">
             <div>
@@ -59,15 +59,15 @@ pub fn manage_guilds(guilds: RwSignal<Vec<Guild>>) -> impl IntoView {
                     <tbody class="divide-y-1 divide-neutral-600">
                         <ForEnumerate
                             each=move || guilds.get()
-                            key=|guild| guild.clone()
+                            key=|guild| guild.get()
                             children=move |idx, guild| {
                                 view! {
                                     <tr class="divide-x-1 divide-neutral-600">
-                                        <td class="border-b-1 border-neutral-600 p-2 font-mono size-fit">"["{guild.prefix.clone()}"]"</td>
-                                        <td class="border-b-1 border-neutral-600 p-2">{guild.name.clone()}</td>
+                                        <td class="border-b-1 border-neutral-600 p-2 font-mono size-fit">"["{guild.get().prefix}"]"</td>
+                                        <td class="border-b-1 border-neutral-600 p-2">{guild.get().name}</td>
                                         <td class="border-b-1 border-neutral-600 p-2 size-fit">
-                                            <span class="pr-2" style:color={guild.hex_color()}>"⬤"</span>
-                                            {guild.hex_color()}
+                                            <span class="pr-2" style:color={guild.read().hex_color()}>"⬤"</span>
+                                            {guild.read().hex_color()}
                                         </td>
                                         // class="text-neutral-600" - help the tailwind cli a bit
                                         <td class="border-b-1 border-neutral-600 flex p-2 gap-1 size-fit" class:text-neutral-600={move || idx.read() == 0}>
@@ -89,7 +89,15 @@ pub fn manage_guilds(guilds: RwSignal<Vec<Guild>>) -> impl IntoView {
                                             </svg>
 
                                             // delete button
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6"
+                                                on:click={ move |_|
+                                                    if idx.read() != 0 {
+                                                        guilds.update(|guilds| {
+                                                            guilds.remove(idx.get());
+                                                        });
+                                                    }
+                                                }
+                                            >
                                                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
                                             </svg>
                                         </td>
