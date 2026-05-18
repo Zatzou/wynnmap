ed2afb4c70ec745e6d8d4ac88f50287e5b5f49fd
Zatzou <zatzou@zatzou.com>
2026-05-18 19:35:06 +0300

    checkbox css

--- src/components/checkbox.rs
+++ src/components/checkbox.rs
@@ -7,15 +7,15 @@ pub fn Checkbox(
     #[prop(optional)] children: Option<Children>,
 ) -> impl IntoView {
     view! {
-        <label for=id class="group flex items-center cursor-pointer select-none">
-            // checkbox
-            <input id=id type="checkbox" bind:checked=checked class="appearance-none relative peer mr-2 h-5 w-5 rounded-sm text-neutral-200 bg-neutral-700 group-hover:bg-neutral-500" />
-            // checkmark
-            <div class="absolute justify-center items-center h-5 w-5 hidden peer-checked:flex">
-                <div class="w-[0.4em] h-[0.75em] mb-0.5 rotate-45 border-white border-0 border-r-3 border-b-3" />
+        <div class="checkbox">
+            <input id=id type="checkbox" bind:checked=checked/>
+            <div class="checkmark">
+                <div/>
             </div>
-            // label
-            <p>{children.map(|c| c())}</p>
-        </label>
+
+            <label for=id>
+                {children.map(|c| c())}
+            </label>
+        </div>
     }
 }
