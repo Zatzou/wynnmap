49a454dd6c2426a1847af6615db8a24764596e84
Zatzou <zatzou@zatzou.com>
2026-06-16 00:12:08 +0300

    gather mode node filtering

--- src/modes/gather/mod.rs
+++ src/modes/gather/mod.rs
@@ -1,12 +1,13 @@
-use std::{collections::BTreeMap, time::Duration};
+use std::collections::BTreeMap;
 
 use leptos::{prelude::*, task::spawn_local};
-use wynnmap_types::gather::GatherSpots;
+use wynnmap_types::gather::{GatherSpots, Material};
 
 use crate::{
-    components::{sidebar::Sidebar, sidecard::SideCard},
+    components::{checkbox::Checkbox, sidebar::Sidebar, sidecard::SideCard},
     datasource,
     modes::gather::noderender::NodeRenderer,
+    settings::use_toggle,
     wynnmap::{WynnMap, maptile::DefaultMapTiles},
 };
 
@@ -38,11 +39,58 @@ pub fn GatherMap() -> impl IntoView {
     let mouse_pos = RwSignal::new(None);
     let hovered = RwSignal::new(Vec::new());
 
+    let toggles = RwSignal::new(Vec::new());
+
+    Effect::new(move |_| {
+        toggles.update(|toggles| {
+            toggles.clear();
+
+            for (n, mat) in nodes.read().resources.iter().enumerate() {
+                toggles.push(MatToggle {
+                    mat: mat.clone(),
+                    toggle: use_toggle(format!("gather-mat-{}", mat.name), true),
+                    count: nodes
+                        .read()
+                        .spots
+                        .iter()
+                        .filter(|s| s.resource == n)
+                        .count(),
+                })
+            }
+        });
+    });
+
+    let hidelist = Memo::new(move |_| {
+        let mut list = Vec::new();
+
+        for toggle in toggles.read().iter() {
+            if !toggle.toggle.get() {
+                list.push(toggle.mat.name.clone())
+            }
+        }
+
+        list
+    });
+
+    let show_all = move |_| {
+        for t in toggles.read().iter() {
+            t.toggle.set(true);
+        }
+    };
+
+    let show_none = move |_| {
+        for t in toggles.read().iter() {
+            t.toggle.set(false);
+        }
+    };
+
+    let search_str = RwSignal::new(String::new());
+
     view! {
         <WynnMap>
             <DefaultMapTiles grayscale=true />
 
-            <NodeRenderer nodes data mouse_pos hovered />
+            <NodeRenderer nodes data mouse_pos hovered hidden={hidelist} />
         </WynnMap>
 
         <SideCard hover=true>
@@ -51,12 +99,62 @@ pub fn GatherMap() -> impl IntoView {
             </div>
             {move || hovered.get().into_iter().map(|node| view!{
                 <div>
-                    <span>{node.res.name}</span>
+                    <span>{titlecase(node.res.name)}</span>
                 </div>
             }).collect::<Vec<_>>()}
         </SideCard>
 
         <Sidebar>
+            <div class="matsearch">
+                <div class="buttons">
+                    <button on:click=show_all>"Show all"</button>
+                    <button on:click=show_none>"Show none"</button>
+                </div>
+                <input type="text" placeholder="Search materials" bind:value=search_str/>
+            </div>
+            <div class="mattoggles">
+                <div/>
+                <span>"Material"</span>
+                <span>"Level"</span>
+                <span>"Count"</span>
+                <For
+                    each=move || toggles.get().into_iter().filter(move |t| t.mat.name.contains(&search_str.read().to_ascii_uppercase()))
+                    key=move |val| val.mat.clone()
+                    children=move |val| view! {
+                        <ToggleComponent toggle=val/>
+                    }
+                />
+            </div>
         </Sidebar>
+    }
+}
+
+#[derive(Clone)]
+struct MatToggle {
+    mat: Material,
+    toggle: RwSignal<bool>,
+    count: usize,
+}
+
+#[component]
+fn ToggleComponent(toggle: MatToggle) -> impl IntoView {
+    view! {
+        <Checkbox id=toggle.mat.name.as_ref() checked=toggle.toggle>
+            <span>{titlecase(toggle.mat.name)}</span>
+            <span>Lv. {toggle.mat.level}</span>
+            <span>{toggle.count}</span>
+        </Checkbox>
+    }
+}
+
+/// Format a string in Titlecase
+fn titlecase(name: impl AsRef<str>) -> String {
+    let mut c = name.as_ref().chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f
+            .to_uppercase()
+            .chain(c.flat_map(|c| c.to_lowercase()))
+            .collect(),
     }
 }
