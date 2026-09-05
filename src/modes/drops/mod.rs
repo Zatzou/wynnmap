f0544015d89e9bb5dff5c1802e18b28326ed499e
Zatzou <zatzou@zatzou.com>
2026-09-05 20:21:45 +0300

    hover infocard

--- src/modes/drops/mod.rs
+++ src/modes/drops/mod.rs
@@ -1,14 +1,19 @@
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    sync::Arc,
+};
+
 use leptos::{prelude::*, task::spawn_local};
 use wynnmap_types::{
     color::Color,
-    drops::{Item, ItemDrops},
+    drops::{DropArea, Item, ItemDrops},
 };
 
 use crate::{
     components::{sidebar::Sidebar, sidecard::SideCard},
     datasource,
     modes::drops::spotrender::SpotRenderer,
-    wynnmap::{WynnMap, maptile::WithDefaultMapTiles},
+    wynnmap::{WynnMap, context::RelMousePos, maptile::WithDefaultMapTiles},
 };
 
 mod spotrender;
@@ -33,14 +38,52 @@ pub fn DropsMap() -> impl IntoView {
     let search_str = RwSignal::new(String::new());
     let selected_item = RwSignal::new(None::<Item>);
 
+    let RelMousePos(mouse_rel) = expect_context();
+    let hovered_spots = Memo::new(move |_| {
+        let mut spots = BTreeMap::new();
+
+        if let Some(item) = &*selected_item.read()
+            && let Some(rel) = mouse_rel.get()
+        {
+            for (name, areas) in &item.dropped_by {
+                let matched = areas
+                    .iter()
+                    .filter(|spot| spot.within(rel, 5))
+                    .cloned()
+                    .collect::<BTreeSet<_>>();
+
+                if !matched.is_empty() {
+                    spots.insert(name.clone(), matched);
+                }
+            }
+        }
+
+        spots
+    });
+
+    let selected_spots = RwSignal::new(BTreeMap::new());
+
+    let onclick = Callback::new(move |_| {
+        selected_spots.set(hovered_spots.get());
+    });
+
     view! {
-        <WynnMap>
+        <WynnMap onclick>
             <WithDefaultMapTiles grayscale=true />
             <SpotRenderer selected_item/>
         </WynnMap>
 
-        <SideCard hover=true>
-        </SideCard>
+        <Show when=move || selected_spots.read().is_empty()>
+            <SideCard hover=true>
+                <For
+                    each=move || hovered_spots.get()
+                    key=move |spot| spot.clone()
+                    let(spot)
+                >
+                    <DropInfoCard spot/>
+                </For>
+            </SideCard>
+        </Show>
 
         <Sidebar>
             {move || if let Some(selected) = selected_item.get() {
@@ -49,6 +92,18 @@ pub fn DropsMap() -> impl IntoView {
                 view! { <DropSearch itemdrops selected_item search_str/> }.into_any()
             }}
         </Sidebar>
+
+        <Show when=move || !selected_spots.read().is_empty()>
+            <SideCard on_close=move |_| selected_spots.set(BTreeMap::new())>
+                <For
+                    each=move || selected_spots.get()
+                    key=move |spot| spot.clone()
+                    let(spot)
+                >
+                    <DropInfoCard spot/>
+                </For>
+            </SideCard>
+        </Show>
     }
 }
 
@@ -128,6 +183,28 @@ fn ItemSelected(selected: Item, back: impl Fn() + 'static) -> impl IntoView {
                         </div>
                     }
                 }).collect::<Vec<_>>()}
+            </div>
+        </div>
+    }
+}
+
+#[component]
+fn DropInfoCard(
+    #[prop(name = "spot")] (name, spots): (Arc<str>, BTreeSet<DropArea>),
+) -> impl IntoView {
+    let col = Color::from_hash(name.as_bytes()).to_rgb_values();
+
+    view! {
+        <div class="dropinfocard" style:--col=col>
+            <div/>
+            <div>
+                <h2>{name}</h2>
+                {spots.into_iter().map(|area| {
+                    let [x, y, z] = area.location;
+                    view! {
+                        <p>"X: "{x}" Y: "{y}" Z: "{z}" radius: "{area.radius}</p>
+                    }}).collect::<Vec<_>>()
+                }
             </div>
         </div>
     }
--- wynnmap-types/src/drops.rs
+++ wynnmap-types/src/drops.rs
@@ -22,6 +22,7 @@ pub struct DropArea {
 }
 
 impl From<[i32; 4]> for DropArea {
+    #[inline]
     fn from(value: [i32; 4]) -> Self {
         let [x, y, z, r] = value;
 
@@ -29,5 +30,19 @@ impl From<[i32; 4]> for DropArea {
             location: [x, y, z],
             radius: r,
         }
+    }
+}
+
+impl DropArea {
+    #[inline]
+    pub fn within(&self, pos: [i32; 2], min_r: i32) -> bool {
+        let [x, _, y] = self.location;
+
+        let dist_x = x.abs_diff(pos[0]);
+        let dist_y = y.abs_diff(pos[1]);
+
+        let r = self.radius.max(min_r);
+
+        dist_x.pow(2) + dist_y.pow(2) <= r.pow(2) as u32
     }
 }
